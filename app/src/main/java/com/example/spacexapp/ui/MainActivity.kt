c1271bf5519575f83f4e5aedf6fd1573ae4d7808
Andres741 <aconderodriguez272@gmail.com>
2022-09-10 17:17:07 +0200

    GraphQL added.

--- app/src/main/java/com/example/spacexapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/spacexapp/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.spacexapp
+package com.example.spacexapp.ui
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
@@ -9,6 +9,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import com.example.spacexapp.R
 import com.example.spacexapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
