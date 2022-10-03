1c783804174c1747b5bc6346e0e483df6013d6c0
Andres741 <aconderodriguez272@gmail.com>
2022-10-03 16:11:37 +0200

    saveImageToStorage method removed from MainActivity.kt

--- app/src/main/java/com/example/spacexapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/spacexapp/ui/MainActivity.kt
@@ -1,27 +1,15 @@
 package com.example.spacexapp.ui
 
-import android.content.ContentValues
-import android.content.Context
-import android.graphics.Bitmap
-import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import android.os.Environment
-import android.provider.MediaStore
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.example.spacexapp.R
 import com.example.spacexapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
-import java.io.File
-import java.io.FileOutputStream
-import java.io.OutputStream
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -51,26 +39,5 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
-    }
-
-    private fun saveImageToStorage(context: Context, bitmap: Bitmap) {
-        val imageOutStream = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            val values = ContentValues()
-            values.put(MediaStore.Images.Media.DISPLAY_NAME, "image_screenshot.jpg")
-            values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
-            values.put(MediaStore.Images.Media.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
-            val uri: Uri =
-                context.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values) ?: return
-
-            contentResolver.openOutputStream(uri)
-        } else {
-            val imagesDir =
-                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString()
-
-            val image = File(imagesDir, "image_screenshotjpg")
-            FileOutputStream(image)
-        }
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, imageOutStream)
-        imageOutStream?.close()
     }
 }
