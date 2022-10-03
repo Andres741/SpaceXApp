d7cf65e980edea6b9d21a2c1b3a739cda5cbed47
Andres741 <aconderodriguez272@gmail.com>
2022-10-03 16:13:50 +0200

    saveImageToStorage function removed from extensions.kt

--- app/src/main/java/com/example/spacexapp/util/extensions/extensions.kt
+++ app/src/main/java/com/example/spacexapp/util/extensions/extensions.kt
@@ -1,19 +1,10 @@
 package com.example.spacexapp.util.extensions
 
-import android.content.ContentValues
-import android.content.Context
-import android.graphics.Bitmap
-import android.net.Uri
-import android.os.Build
-import android.os.Environment
-import android.provider.MediaStore
 import androidx.lifecycle.*
 import coil.request.ErrorResult
 import coil.request.ImageResult
 import coil.request.SuccessResult
 import kotlinx.coroutines.delay
-import java.io.File
-import java.io.FileOutputStream
 import java.text.SimpleDateFormat
 import kotlin.system.measureTimeMillis
 
@@ -53,32 +44,4 @@ suspend inline fun<T> delayAtLeast(millis: Long, block: () -> T): T {
         delay(millis - it)
     }
     return elem
-}
-
-fun saveImageToStorage(imageName: String? = null, context: Context, bitmap: Bitmap): Boolean {
-    val name = imageName ?: "spacex_image.jpg"
-
-    val imageOutStream = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-
-        val values = ContentValues().apply {
-            put(MediaStore.Images.Media.DISPLAY_NAME, name)
-            put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
-            put(MediaStore.Images.Media.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
-        }
-
-        val uri: Uri =
-            context.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values) ?: return false
-
-        context.contentResolver.openOutputStream(uri) ?: return false
-    } else {
-        val imagesDir =
-            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString()
-
-        val image = File(imagesDir, name)
-        FileOutputStream(image)
-    }
-
-    return imageOutStream.use {
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, imageOutStream)
-    }
 }
